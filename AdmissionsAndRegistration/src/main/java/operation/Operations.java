a714760c4a5783f69dd07ab7d10405828dbb9b31
Danemoto9612 <danielmortor1202tdea@gmail.com>
2025-10-16 13:22:25 -0500

    UPD class Operations

--- AdmissionsAndRegistration/src/main/java/operation/Operations.java
+++ AdmissionsAndRegistration/src/main/java/operation/Operations.java
@@ -16,6 +16,7 @@ public class Operations {
     // Variables para la función de retorno del número fibonacci
     private static int a = 0, b = 1, next;
 
+    // Método para crear un registro nuevo
     public static void createRecord() {
 
         if (lenRecords < 50) {
@@ -51,6 +52,7 @@ public class Operations {
             System.out.println("Fecha de ingreso: " + records[lenRecords].enterDate);
             System.out.println("----------------------------------------------------------------------------");
             System.out.println("Hora de ingreso: " + records[lenRecords].enterTime);
+            
             // Ciclo para verificar si el usuario requiere atención especial
             while (true) {
 
@@ -59,10 +61,12 @@ public class Operations {
                 System.out.print("Requiere atención especial (1-Si 2-No): ");
                 option = sc.nextLine();
                 
+                // Condición lógica para confirmar si requiere atención especial
                 if (option.equals("1")) {
 
                     System.out.println("----------------------------------------------------------------------------");
                     records[lenRecords].specialService = true;
+                    // Retorna un número de la serie Fibonacci para usar cómo código de atención especial
                     records[lenRecords].fibonacciNumber = returnFibonacci();
                     System.out.println("Atencion especial registrada con código: " + records[lenRecords].fibonacciNumber);
                     break;
@@ -78,30 +82,60 @@ public class Operations {
                 }
             }
             
+            // Actualiza el valor de lenRecords
             lenRecords++;
         } else {
 
+            // El vector de registros se encuentra lleno
             System.out.println("----------------------------------------------------------------------------");
             System.out.println("       I M P O S I B L E   A T E N D E R   M A S   U S U A R I O S");
         }
     }
 
+    // Método para mostrar los registros existentes
     public static void showRecords() {
+        
+        // Verificar que existen registros para poder mostrarlos
+        if (lenRecords > 0) {
+            
+            // Títulos de la tabla
+            System.out.println("+------+--------+--------+-------+-------+---------+-----------+-----------+");
+            System.out.println("|Nombre| F.Ingre| H.Ingre| F.Aten| H.Aten| Atendido| S.Especial| Código F. |");
+            System.out.println("+------+--------+--------+-------+-------+---------+-----------+-----------+");
+            
+            // Ciclo para mostrar cada registro almacenado
+            for (int i = 0; i < lenRecords; i++) {
+               
+                System.out.println(records[i].name + " " + records[i].enterDate + " " + records[i].enterTime
+                + " " + records[i].serviceDate + " " + records[i].serviceTime + " " + records[i].service
+                + " " + records[i].specialService + " " + records[i].fibonacciNumber);
+            }
+        } else {
+            
+            // No existen registros
+            System.out.println("----------------------------------------------------------------------------");
+            System.out.println("           N O   S E   H A N   C R E A D O   R E G I S T R O S");
+        }
     }
 
+    // Método para atender a la siguiente persona en la fila
     public static void serveNextPerson() {
     }
 
+    // Método para calcular el tiempo promedio de atención
     public static void calculateAverageTime() {
     }
 
+    // Método para cambiar de fila
     public static void changeQueue() {
     }
 
+    // Método para calcular atención en un intervalo dado
     public static void attentionInterval() {
     }
     
-    public static int returnFibonacci() {
+    // Método interno para retornar un número dentro de la serie Fibonacci
+    private static int returnFibonacci() {
         
         next = a + b;
         a = b;
